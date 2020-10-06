e3b79d3637f35a4d4bf45d2449fe1385358c7a16
Ismael Ponce <ImDevPonce@outlook.com>
2020-10-06 02:38:11 -0500

    before gradient

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
-const LoginScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>LoginScreen.js</Text>
-    </View>
-  );
+const LoginScreen = (props) => {
+  const [state, setState] = useState({name: ''});
+  const {name} = state;
+
+  const continueToChat = () => {
+    props.navigation.navigate('Chat', name);
+  };
+  return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#D9AFD9',
   },
 });
 export default LoginScreen;
