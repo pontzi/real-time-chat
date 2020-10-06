27f35229aae621bb8a22416202f0308bc64e1331
Ismael Ponce <ImDevPonce@outlook.com>
2020-10-06 02:15:19 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+//Screens
+import ChatScreen from './screens/ChatScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
